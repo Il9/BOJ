e11edbe570d0927aaabd377d4ff7eb5087893877
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 08:18:18 +0900

    Solved CLASS_1/10871

---
+++ CLASS_1/10871/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [numberString1, numberString2] = input;
+
+const [, number1] = numberString1.split(' ').map(Number);
+const numbers = numberString2.split(' ').map(Number);
+
+console.log(numbers.filter(number => number1 > number).join(' '));
