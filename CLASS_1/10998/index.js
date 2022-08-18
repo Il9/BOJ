972e0af3f3826e24f51c083a5632489afc92b3af
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 08:30:05 +0900

    Solved CLASS_1/10998

---
+++ CLASS_1/10998/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [numberString] = input;
+
+const [number1, number2] = numberString.split(' ').map(Number);
+
+console.log(number1 * number2);
