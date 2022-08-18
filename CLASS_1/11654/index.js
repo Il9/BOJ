2032c80edd1307bbaa8cfaa61f093a5d6ecc3fce
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 08:30:17 +0900

    Solved CLASS_1/11654

---
+++ CLASS_1/11654/index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [string] = input;
+
+console.log(string.charCodeAt());
