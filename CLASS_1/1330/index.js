091dd00422ab6bb651b5b51fe7e98992bfed356d
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-18 22:38:26 +0900

    Solved CLASS_1/1330

---
+++ CLASS_1/1330/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const [numberString] = input;
+
+const [number1, number2] = numberString.split(' ').map(Number);
+
+if (number1 < number2) {
+  console.log('<');
+} else if (number1 > number2) {
+  console.log('>');
+} else if (number1 === number2) {
+  console.log('==');
+}
