2110051d527c0c0dc3df9ec83a82777d84ae6c43
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 08:08:49 +0900

    Solved CLASS_1/10809

---
+++ CLASS_1/10809/index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const [string] = input;
+
+const alphabets = Array.from(Array(26)).map((_, i) => String.fromCharCode(97 + i));
+
+console.log(alphabets.map(alphabet => string.indexOf(alphabet)).join(' '));
