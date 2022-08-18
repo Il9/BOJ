45b53fe3adb5fb2b60f3bbc0f52cbd0147713981
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-18 22:52:12 +0900

    Solved CLASS_1/2739

---
+++ CLASS_1/2739/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const [numberString] = input;
+
+const [number] = numberString.split(' ').map(Number);
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${number} * ${i} = ${number * i}`);
+}
