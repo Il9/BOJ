ebd87f76a062880c0cd039ec8ee9a9128b6ddc55
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 09:07:03 +0900

    Solved CLASS_1/11720

---
+++ CLASS_1/11720/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [, numberString] = input;
+
+const numbers = numberString.split('').map(Number);
+
+console.log(numbers.reduce((acc, cur) => acc + cur, 0));
