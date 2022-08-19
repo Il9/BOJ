42c4daafc115b134a66769bc3f736beed0529f2b
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 10:30:35 +0900

    Solved CLASS_1/2920

---
+++ CLASS_1/2920/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [numberString] = input;
+
+if (numberString === '1 2 3 4 5 6 7 8') {
+  console.log('ascending');
+} else if (numberString === '8 7 6 5 4 3 2 1') {
+  console.log('descending');
+} else {
+  console.log('mixed');
+}
