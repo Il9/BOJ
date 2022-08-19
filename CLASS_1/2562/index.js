cff0049145da8c061f5c689863971ca39a29e1a1
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 09:17:01 +0900

    Solved CLASS_1/2562

---
+++ CLASS_1/2562/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [...numberStrings] = input;
+
+const numbers = numberStrings.map(Number);
+
+const max = numbers.reduce((acc, cur, index) => {
+  if (acc.number >= cur) {
+    return acc;
+  }
+
+  return {
+    index,
+    number: cur    
+  }
+}, {index: 0, number: numbers[0]});
+
+console.log(max.number);
+console.log(max.index + 1);
