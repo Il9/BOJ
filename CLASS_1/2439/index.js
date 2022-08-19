c041cb001f86e1f2ce1a407c765225eb4633bdfa
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 09:00:45 +0900

    Solved CLASS_1/2439

---
+++ CLASS_1/2439/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [numberString] = input;
+
+const [number1] = numberString.split(' ').map(Number);
+
+for (let i = 1; i <= number1; i++) {
+  console.log(`${' '.repeat(number1 - i)}${'*'.repeat(i)}`);
+}
