f65d9ba5d14a9949e6fed19a704bc90f42418705
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 11:18:50 +0900

    Solved CLASS_1/1546

---
+++ CLASS_1/1546/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [lengthString, numbersString] = input;
+const numbers = numbersString.split(' ').map(Number);
+
+const max = numbers.reduce((acc, cur) => acc < cur ? cur : acc, 0);
+
+console.log(numbers.reduce((acc, number) => acc + (number / max * 100), 0) / Number(lengthString));
