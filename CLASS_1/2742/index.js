d54667ae7b941a6dc69df36301889a828f9af4c1
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 09:03:05 +0900

    Solved CLASS_1/2742

---
+++ CLASS_1/2742/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [numberString] = input;
+
+const [number] = numberString.split(' ').map(Number);
+
+let text = '';
+
+for (let i = number; i > 0; i--) {
+  text += `\n${i}`
+}
+
+console.log(text.substring(1));
