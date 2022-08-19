f2798bfe9f011046305625c551a8029f7e81eca9
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-19 09:47:00 +0900

    Solved CLASS_1/2577

---
+++ CLASS_1/2577/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [number1, number2, number3] = input.map(Number);
+
+const number = number1 * number2 * number3;
+
+for (let i = 0; i <= 9; i++) {
+  console.log((number.toString().match(new RegExp(i, 'g')) ?? []).length);
+}
