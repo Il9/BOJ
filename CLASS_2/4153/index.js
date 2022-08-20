8dafac7fc069ebdbc6845a3c42a755b85f5b2363
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-20 15:00:14 +0900

    Solved CLASS_2/4153

---
+++ CLASS_2/4153/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const numbersStrings = input;
+
+numbersStrings.slice(0, -1).forEach(numbersString => {
+  const widths = numbersString.split(' ').map(Number);
+
+  const c = Math.max(...widths);
+  const [a, b] = widths.filter(width => width !== c);
+
+  console.log(((a * a + b * b) === (c * c)) ? 'right' : 'wrong');
+})
