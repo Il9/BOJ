866d38692fcd2aea7bae47ef163b175db04643eb
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-24 07:58:04 +0900

    Solved CLASS_2/11050

---
+++ CLASS_2/11050/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [NKString] = input;
+
+const [N, K] = NKString.split(' ').map(Number);
+
+function factorial (number) {
+  if (!number) return 1;
+
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(N) / (factorial(N - K) * factorial(K)));
