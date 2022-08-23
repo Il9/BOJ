c285d40fe4f19e8523f6b5a0d66f66f3d024c83b
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-24 07:59:08 +0900

    Solved CLASS_2/10989

---
+++ CLASS_2/10989/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [lengthString, ...numberStrings] = input;
+
+const length = Number(lengthString);
+const numbers = numberStrings.map(Number);
+
+const arr = [];
+
+for (let i = 0; i < length; i++) {
+  arr[numbers[i]] = (arr[numbers[i]] ?? 0) + 1;
+}
+
+for (let j = 1; j <= arr.length; j++) {
+  for (let k = 0; k < arr[j]; k++) {
+    console.log(j);
+  }
+}
