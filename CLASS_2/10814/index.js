297309d4c7dea5d8034bafdc5a32c0b0ef64226e
Myeongjin Choi <rntpwn90@gmail.com>
2022-09-01 08:47:14 +0900

    Solved CLASS_2/10814

---
+++ CLASS_2/10814/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [, ...members] = input;
+
+members.sort((a, b) => {
+  const age1 = Number(a.match(/^\d+/g)[0]);
+  const age2 = Number(b.match(/^\d+/g)[0]);
+
+  return age1 === age2 ? 1 : age1 > age2 ? 1 : -1
+});
+
+console.log(members.join('\n'));
