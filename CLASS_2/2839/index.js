8772640a00a27189fad37aebcb5bad4ab797d5a0
Myeongjin Choi <rntpwn90@gmail.com>
2022-09-07 07:03:53 +0900

    Solved CLASS_2/2839

---
+++ CLASS_2/2839/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [NString] = input;
+
+const N = Number(NString);
+
+if (N <= 7) {
+  if ([3, 5].includes(N)) {
+    console.log(1);
+  } else if (N === 6) {
+    console.log(2);
+  } else {
+    console.log(-1);
+  }
+} else {
+  console.log(Math.floor(N / 5) + [0, 1, 2, 1, 2][N % 5]);
+}
