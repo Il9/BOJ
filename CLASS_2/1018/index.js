915e52f109ca76b3838fda5b845f58d07dc344a3
Myeongjin Choi <rntpwn90@gmail.com>
2022-09-19 08:54:39 +0900

    Solved CLASS_2/1018

---
+++ CLASS_2/1018/index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [NMString, ...rows] = input;
+
+const [N, M] = NMString.split(' ').map(Number);
+
+const SIZE = 8;
+
+let answer = null;
+
+function getPaintCount(board) {
+  const colorMap = { 'B': 'W', 'W': 'B' };
+
+  return Math.min(
+    ...['B', 'W'].map(firstColor => {
+      let colorBuffer = firstColor;
+      let paintCount = 0;
+
+      for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+          if (board[i][j] !== colorBuffer) paintCount = paintCount + 1;
+          colorBuffer = colorMap[colorBuffer];
+        }
+        colorBuffer = colorMap[colorBuffer];
+      }
+
+      return paintCount;
+    })
+  );
+}
+
+for (let i = SIZE; i <= N; i++) {
+  for (let j = SIZE; j <= M; j++) {
+    const board = rows.slice(i - SIZE, i).map(row => row.slice(j - SIZE, j));
+    const paintCount = getPaintCount(board);
+
+    answer = answer === null || answer > paintCount ? paintCount : answer;
+  }
+}
+
+console.log(answer);
