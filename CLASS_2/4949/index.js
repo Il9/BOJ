56d84023e2914e85fe5b215a9dec536e12178089
Myeongjin Choi <rntpwn90@gmail.com>
2022-10-22 15:45:35 +0900

    Solved CLASS_2/4949

---
+++ CLASS_2/4949/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const answer = input.slice(0, -1).map(string => {
+  let s = string.replace(/[^(\(|\)|\[|\])]/g, '');
+
+  while (s.includes('()') || s.includes('[]')) {
+    s = s.replace(/((\(\))|(\[\]))/g, '');
+  }
+
+  return s.length ? 'no' : 'yes';
+});
+
+console.log(answer.join('\n'));
