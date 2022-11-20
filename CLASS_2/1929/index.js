e68c23ec3f30118bf6005003e4ddd809f6244a03
Myeongjin Choi <rntpwn90@gmail.com>
2022-11-20 21:55:58 +0900

    Solved CLASS_2/1929

---
+++ CLASS_2/1929/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const inputString = fs.readFileSync("/dev/stdin").toString().trim();
+const input = inputString.split('\n');
+
+const [MNString] = input;
+
+const [M, N] = MNString.split(' ').map(Number);
+
+let numbers = Array.from({length: N - 1}, (_, index) => index + 2);
+let target = 0;
+let prevNumberLength = numbers.length;
+
+do {
+  const primeNumber = numbers[target++];
+
+  prevNumberLength = numbers.length;
+  numbers = numbers.filter(number => number === primeNumber || number % primeNumber !== 0);
+} while (prevNumberLength !== numbers.length)
+
+console.log(numbers.filter(number => M <= number && number <= N).join('\n'));
